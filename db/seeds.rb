9523bb6571bc8fd9bf43de449d00e39f8203b14b
YOUR NAME <muvanlier@gmail.com>
2017-07-12 19:32:41 +0200

    added songs seed and interlinked it with the show page

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,7 @@
+Song.destroy_all
+Artist.destroy_all
+
+
 artist1 = Artist.create!( {name: "Bob Dylan"} )
 artist2 = Artist.create!( {name: "Craig David"} )
 artist3 = Artist.create!( {name: "Tupac"} )
@@ -6,3 +10,12 @@ artist5 = Artist.create!( {name: "Allen Rayman"} )
 artist6 = Artist.create!( {name: "Charles Bradley"} )
 artist7 = Artist.create!( {name: "David Bowie"} )
 artist8 = Artist.create!( {name: "Jim Morrissey"} )
+
+songs1 = Song.create!( {name: "Like a Rolling Stone", artist: artist1})
+songs2 = Song.create!( {name: "7 Days", artist: artist2} )
+songs3 = Song.create!( {name: "Oldskool", artist: artist3} )
+songs4 = Song.create!( {name: "Three little birds", artist: artist4} )
+songs5 = Song.create!( {name: "Tennesse", artist: artist5} )
+songs6 = Song.create!( {name: "Changes",  artist: artist6} )
+songs7 = Song.create!( {name: "Heroes", artist: artist7} )
+songs8 = Song.create!( {name: "There's a Light That Never Goes Out", artist: artist8} )
